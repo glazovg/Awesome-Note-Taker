f4a70c2b3d53179eb9bbfabd49e1c41077d120ea
Guillermo Lazo <glazovg@gmail.com>
2022-02-12 17:21:32 -0600

    Adding routes

---
+++ routes/notes.js
@@ -0,0 +1,58 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// DELETE Route for a specific note
+notes.delete('/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+
+  if (noteId === 'undefined') {
+    res.status(404).send('Note ID not found.');
+    alert('Note ID not found.');
+    return;
+  }
+
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((note) => note.id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Note ${noteId} has been deleted 🗑️`);
+    });
+});
+
+// POST Route for a new UX/UI note
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = notes;
